e320cbed16876ab978221234775bddb443c7b603
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2023-02-02 12:59:27 +0000

    feature and rspec

---
+++ spec/controller/fee_calculator_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe FeeCalculatorController, :type => :controller do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+end
---
+++ spec/features/fee_calculator_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Fee calculator', type: :feature do
+  scenario 'Visit the page' do
+    visit '/'
+
+    expect(page).to have_content('LAA Fee Calculator User Interface')
+  end
+
+  scenario 'Visit the page' do
+    visit '/'
+
+    expect(page).to have_content('AGFS Fee Scheme 9')
+  end
+end
