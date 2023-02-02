aa35f672b61dd60df612ac8d02f3e36bfa762ec5
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2023-02-02 12:54:55 +0000

    retrieve fee schemes from service class

--- app/controllers/fee_calculator_controller.rb
+++ app/controllers/fee_calculator_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class FeeCalculatorController < ApplicationController
+
   def index
     @message = 'Fee scheme'
+    @fee_schemes = FeeCalculator.new.fee_schemes
   end
 end
