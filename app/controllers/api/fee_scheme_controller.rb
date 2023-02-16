a4c11a50921f7e6c37359a0b8ef689b111c869e2
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2023-02-16 15:26:07 +0000

    add javascript to fetch the selected fee scheme id

---
+++ app/controllers/api/fee_scheme_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  class FeeSchemeController < ApplicationController
+    def show
+      render json: {}
+    end
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,25 @@ import { initAll } from 'govuk-frontend'
 initAll()
 
 console.log('Hello world')
+
+window.onload = function(){
+var feeSelect = document.getElementById("fee-scheme-fee-scheme-field")
+feeSelect.onchange = onChangeFunction
+}
+
+function onChangeFunction(){
+var feeIndex = document.getElementById("fee-scheme-fee-scheme-field").value;
+var feeSelected = document.getElementById("fee-scheme-fee-scheme-field")
+var chosenOption =feeSelected.querySelectorAll(`option[value="${feeIndex}"]`);
+console.log(chosenOption[0].value)
+
+
+
+
+
+fetch(`/api/fee_scheme/${chosenOption[0].value}`)
+  .then((response) => response.json())
+  .then((data) => { console.log(data); });
+}
+
+
